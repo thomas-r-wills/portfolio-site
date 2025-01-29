79b1c31423d962b8f1eecc49ae0e377de615cf91
“Thomas <tw8307542@gmail.com>
2025-01-29 22:29:25 +1100

    profile photo fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+
 
 export default function Home() {
   const [displayContent, setDisplayContent] = useState("name");
